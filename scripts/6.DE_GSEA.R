61394fbea03b6c74329e6d8d6db0b6482c6c7d43
stephaniesaab <stephaniesaab5@gmail.com>
2026-04-11 18:43:43 -0400

    Removing ClusterProfiler

--- scripts/6.DE_GSEA.R
+++ scripts/6.DE_GSEA.R
@@ -5,7 +5,7 @@ library(Seurat)
 library(SingleCellExperiment) 
 library(SingleR)
 library(DESeq2) #For the DE
-library(clusterProfiler)   
+#library(clusterProfiler) Not in the narval cluster
 library(org.Mm.eg.db)        
 library(BiocParallel)
 
@@ -69,7 +69,7 @@ pseudo_srt <- AggregateExpression(srt,
                                                "time", "biosample_id", "mouse_id"))
 ncol(pseudo_srt) #Should have 3x as many columns (3x12)
 #Create comparison ID on pseudobulk object
-pseudo_srt$cell_time_organ <- paste0(pseudo_srt$cell_type,
+pseudo_srt$cell_time_organ <- paste(pseudo_srt$cell_type,
                                      pseudo_srt$time,
                                      pseudo_srt$organ_custom, sep = "_")
 Idents(pseudo_srt) <- "cell_time_organ"
@@ -82,12 +82,12 @@ Idents(pseudo_srt) <- "cell_time_organ"
 #org = organ
 #For mouse id naming conventions with paper: RT = RM, ET = OM
 comparisons <- list(
-  list(ct="Basal_3", tp="D05", org="RM"),
-  list(ct="DC_8",    tp="D05", org="RM"),
-  list(ct="OSN_Merged", tp="D05", org="OM"),
-  list(ct="Basal_3", tp="D14", org="RM"),
-  list(ct="DC_8",    tp="D14", org="RM"),
-  list(ct="OSN_Merged", tp="D14", org="OM")
+  list(ct="IFN-Stim Basal", tp="D05", org="RM"),
+  list(ct="IFN-Stim DC",    tp="D05", org="RM"),
+  list(ct="Mature OSN", tp="D05", org="OM"),
+  list(ct="IFN-Stim Basal", tp="D14", org="RM"),
+  list(ct="IFN-Stim DC",    tp="D14", org="RM"),
+  list(ct="Mature OSN", tp="D14", org="OM")
 )
 
 for (item in comparisons) {
@@ -110,152 +110,23 @@ for (item in comparisons) {
     #Save the comparison
     write.csv(bulk_de, paste0("../data/Pseudobulk_DESeq2_", g1, "_vs_Naive.csv"))
     
-    #GSEA (Ranked by log2FoldChange from DESeq2)
-    gene_list <- sort(setNames(bulk_de$avg_log2FC, 
-                               rownames(bulk_de)), 
-                      decreasing = TRUE)
-    
-    gsea_res <- gseGO(geneList = gene_list, 
-                      OrgDb = org.Mm.eg.db, 
-                      keyType = 'SYMBOL', 
-                      ont = "BP",
-                      pvalueCutoff = 0.05,
-                      minGSSize = 10)
-    
-    if(!is.null(gsea_res) && nrow(as.data.frame(gsea_res)) > 0) {
-      write.csv(as.data.frame(gsea_res), paste0("../data/GSEA_", g1, ".csv"))
-    }
-  } else {
-    message(paste("!!! Skipping:", g1, "or", g2, "not found in data."))
-  }
+  #   #GSEA (Ranked by log2FoldChange from DESeq2)
+  #   gene_list <- sort(setNames(bulk_de$avg_log2FC, 
+  #                              rownames(bulk_de)), 
+  #                     decreasing = TRUE)
+  #   
+  #   gsea_res <- gseGO(geneList = gene_list, 
+  #                     OrgDb = org.Mm.eg.db, 
+  #                     keyType = 'SYMBOL', 
+  #                     ont = "BP",
+  #                     pvalueCutoff = 0.05,
+  #                     minGSSize = 10)
+  #   
+  #   if(!is.null(gsea_res) && nrow(as.data.frame(gsea_res)) > 0) {
+  #     write.csv(as.data.frame(gsea_res), paste0("../data/GSEA_", g1, ".csv"))
+  #   }
+  # } else {
+  #   message(paste("Skipping:", g1, "or", g2, "not found in data."))
+  # }
 }
 message(">>> DONE!")
-
-
-# 
-# # We set logfc.threshold to 0 to get ALL genes for GSEA ranking
-# Idents(srt) <- "cell_condition"
-# de_results <- FindMarkers(srt, 
-#                           ident.1 = "Basal_Influenza", 
-#                           ident.2 = "Basal_Mock",
-#                           logfc.threshold = 0, # Required for GSEA
-#                           min.pct = 0.1)
-# 
-# write.csv(de_results, "../data/DE_Basal_Full_List.csv")
-# 
-# # 4. PREPARE GENE LIST FOR GSEA
-# # We rank genes by their avg_log2FC
-# gene_list <- de_results$avg_log2FC
-# names(gene_list) <- rownames(de_results)
-# gene_list <- sort(gene_list, decreasing = TRUE)
-# 
-# # 5. RUN GSEA
-# # This tells you which biological pathways are "enriched" at the top or bottom of your list
-# gsea_res <- gseGO(geneList      = gene_list,
-#                   OrgDb         = org.Mm.eg.db,
-#                   keyType       = 'SYMBOL',
-#                   ont           = "BP", # Biological Process
-#                   minGSSize     = 10,
-#                   maxGSSize     = 500,
-#                   pvalueCutoff  = 0.05,
-#                   verbose       = FALSE)
-# 
-# # 6. SAVE RESULTS & PLOTS
-# write.csv(as.data.frame(gsea_res), "../data/GSEA_Basal_Pathways.csv")
-# 
-# # Generate the "Running Sum" plot for the top pathway
-# png("../plots/GSEA_Top_Pathway.png", width=10, height=8, units="in", res=300)
-# gseaplot(gsea_res, geneSetID = 1, by = "runningScore", title = gsea_res$Description[1])
-# dev.off()
-# 
-# # Save the final annotated UMAP
-# png("../plots/Final_Annotated_UMAP.png", width=10, height=8, units="in", res=300)
-# DimPlot(srt_rds, reduction = "umap", label = TRUE, repel = TRUE) + 
-#   ggtitle("Annotated Nasal Mucosa Clusters")
-# dev.off()
-# 
-# # Create a combined identity of CellType + Condition
-# srt_rds$cell_condition <- paste0(Idents(srt_rds), "_", srt_rds$condition)
-# Idents(srt_rds) <- "cell_condition"
-# 
-# # Run DE: Influenza vs. Mock specifically in Basal cells (Cluster 3)
-# de_results <- FindMarkers(srt_rds, 
-#                           ident.1 = "IFN-Stim Basal_Influenza", 
-#                           ident.2 = "IFN-Stim Basal_Mock")
-# 
-# write.csv(de_results, "../data/DE_Basal_Infection_vs_Mock.csv")
-# 
-# library(clusterProfiler)
-# library(org.Mm.eg.db)
-# 
-# #USE SCTRANSFORM ASSAY FOR THE UMAP
-# 
-# # Get the genes that are significantly UP in the infected group
-# up_genes <- rownames(de_results[de_results$p_val_adj < 0.05 & de_results$avg_log2FC > 1, ])
-# 
-# # Run the enrichment
-# ego <- enrichGO(gene = up_genes, 
-#                 OrgDb = org.Mm.eg.db, 
-#                 keyType = 'SYMBOL', 
-#                 ont = "BP")
-# 
-# # Plot it
-# png("../plots/Pathway_Enrichment_Basal.png", width=10, height=8, units="in", res=300)
-# dotplot(ego, showCategory=15) + ggtitle("Pathways Up in Infected Basal Cells")
-# dev.off()
-# 
-# # Calculate proportions
-# comp_table <- table(Idents(srt_rds), srt_rds$condition)
-# prop_table <- prop.table(comp_table, margin = 2)
-# 
-# # Save the barplot
-# png("../plots/Cell_Composition_Change.png", width=8, height=6, units="in", res=300)
-# barplot(prop_table, col=rainbow(nrow(prop_table)), 
-#         legend = rownames(prop_table), 
-#         args.legend = list(x = "topright", bty = "n", inset=c(-0.2, 0)),
-#         main="Tissue Shift: Mock vs Influenza")
-# dev.off()
-# 
-# # 1. SETUP PARALLELIZATION
-# # This allows Seurat to use the multiple CPUs you request in Slurm
-# plan("multisession", workers = 16) 
-# options(future.globals.maxSize = 100 * 1024^3) # 100GB limit
-# 
-# # 2. LOAD DATA
-# srt <- readRDS("../data/seurat_final_annotated.rds")
-# 
-# # 3. RUN DIFFERENTIAL EXPRESSION (DE)
-# # Example: Influenza vs Mock in Basal Cells
-# # We set logfc.threshold to 0 to get ALL genes for GSEA ranking
-# Idents(srt) <- "cell_condition"
-# de_results <- FindMarkers(srt, 
-#                           ident.1 = "Basal_Influenza", 
-#                           ident.2 = "Basal_Mock",
-#                           logfc.threshold = 0, # Required for GSEA
-#                           min.pct = 0.1)
-# 
-# write.csv(de_results, "../data/DE_Basal_Full_List.csv")
-# 
-# # 4. PREPARE GENE LIST FOR GSEA
-# # We rank genes by their avg_log2FC
-# gene_list <- de_results$avg_log2FC
-# names(gene_list) <- rownames(de_results)
-# gene_list <- sort(gene_list, decreasing = TRUE)
-# 
-# # 5. RUN GSEA
-# # This tells you which biological pathways are "enriched" at the top or bottom of your list
-# gsea_res <- gseGO(geneList      = gene_list,
-#                   OrgDb         = org.Mm.eg.db,
-#                   keyType       = 'SYMBOL',
-#                   ont           = "BP", # Biological Process
-#                   minGSSize     = 10,
-#                   maxGSSize     = 500,
-#                   pvalueCutoff  = 0.05,
-#                   verbose       = FALSE)
-# 
-# # 6. SAVE RESULTS & PLOTS
-# write.csv(as.data.frame(gsea_res), "../data/GSEA_Basal_Pathways.csv")
-# 
-# # Generate the "Running Sum" plot for the top pathway
-# png("../plots/GSEA_Top_Pathway.png", width=10, height=8, units="in", res=300)
-# gseaplot(gsea_res, geneSetID = 1, by = "runningScore", title = gsea_res$Description[1])

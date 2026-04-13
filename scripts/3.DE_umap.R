1c7c616f0fd463618393a08d12a2de3c73f4528f
Stephanie Saab <85300454+stephaniesaab@users.noreply.github.com>
2026-04-13 12:07:45 -0400

    Update 3.DE_umap.R

--- scripts/3.DE_umap.R
+++ scripts/3.DE_umap.R
@@ -16,15 +16,15 @@ plan("multisession", workers = 4)
 #Run PCA -> Returns Seurat object with PCA calculation stored in reductions slot
 #Calculate 40 PCs but really only will use 16 based on Elbow plot
 srt_rds <- RunPCA(srt_rds, 
-                  assay = "SCT", #Assat PCA is run on is SCTransform
+                  assay = "SCT", #Assay PCA is run on is SCTransform
                   npcs = 40, 
                   verbose = TRUE, #Print the top genes associated with high/low loadings for the PCs
                   seed.use= 42, #Set seed to 42, default
                   )
 
 # # Clustering
-srt_rds <- FindNeighbors(srt_rds, dims = 1:16) #Set cutoff as 17 PCs
-srt_rds <- FindClusters(srt_rds, resolution = 0.4) #34 Clusters at resolution 0.5, a bit much
+srt_rds <- FindNeighbors(srt_rds, dims = 1:16) #Set cutoff as 16 PCs
+srt_rds <- FindClusters(srt_rds, resolution = 0.4) #34 Clusters at resolution 0.5 was a bit much
 srt_rds <- RunUMAP(srt_rds, dims = 1:16)
 
 
